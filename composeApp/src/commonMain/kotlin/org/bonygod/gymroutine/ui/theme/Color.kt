28bae581c26cfe5ac0d1ab435924e506d69d3dd6
BonyGoD <bonygod.dev@gmail.com>
2025-03-07 12:19:25 +0100

    Finalizar login y registro con KTOR y ROOM

---
+++ composeApp/src/commonMain/kotlin/org/bonygod/gymroutine/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package org.bonygod.gymroutine.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val CustomYellow = Color(0xFFF2F862)
+val CustomBlack = Color(0xFF000000)
+val CustomGray = Color(0xFF404040)
+val CustomLightGray = Color(0xFFC1C1C1)
+val CustomWhite = Color(0xFFFFEFEFE)
