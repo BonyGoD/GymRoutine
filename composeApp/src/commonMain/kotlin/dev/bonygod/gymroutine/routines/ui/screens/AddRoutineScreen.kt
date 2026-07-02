da58ed3dc63678fb86b475855f92c001df060e2a
BonyGoD <bonygod.dev@gmail.com>
2026-07-02 18:38:30 +0200

    feat: Añadir scroll a las rutinas

--- composeApp/src/commonMain/kotlin/dev/bonygod/gymroutine/routines/ui/screens/AddRoutineScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/bonygod/gymroutine/routines/ui/screens/AddRoutineScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -130,6 +131,7 @@ fun AddRoutineScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
+                .imePadding()
                 .padding(start = 24.dp, end = 24.dp, bottom = 40.dp),
             verticalArrangement = Arrangement.spacedBy(24.dp),
         ) {
