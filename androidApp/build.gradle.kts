34c4d529fff28d728738b2ff0b2ec0ac1577742a
BonyGoD <bonygod.dev@gmail.com>
2026-08-09 14:23:08 +0200

    feat: Update version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -29,8 +29,8 @@ android {
             libs.versions.android.targetSdk
                 .get()
                 .toInt()
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
     }
 
     packaging {
